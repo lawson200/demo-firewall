34a7e8f94980f6c292cf4071878ff4d23c11d960
lawson200 <160137883+lawson200@users.noreply.github.com>
2025-03-14 10:32:57 +0100

    Update and rename client_setup.sh to setup_client.sh

---
+++ setup_client.sh
@@ -0,0 +1,6 @@
+
+echo " Installation des outils de test (Nmap, Curl)"
+sudo apt update && sudo apt install -y nmap curl net-tools
+
+
+echo "Client prêt pour les tests de connectivité et de sécurité."
